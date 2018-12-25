5680f24a17778cfcf26f8ef3e6a745be80ace331
Rohan Gupta <srgupta5328@gmail.com>
2018-12-25 14:01:42 -0500

    Added integer slice test case practice

--- iterations/repeat.go
+++ iterations/repeat.go
@@ -7,3 +7,14 @@ func Repeat(val string) string {
 	}
 	return character
 }
+
+var list = []int{1, 3, 3, 4, 5, 6, 7}
+
+func PrintList(val []int) []int {
+	var manipulatedList []int
+	for _, v := range list {
+		manipulatedList = append(manipulatedList, v+1)
+	}
+
+	return manipulatedList
+}
--- iterations/repreat_test.go
+++ iterations/repreat_test.go
@@ -10,3 +10,14 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("expected: '%s', got: '%s'", expected, repeated)
 	}
 }
+
+//When comparing lists make sure that it equals nil for test
+func TestPrintLnist(t *testing.T) {
+	testList := PrintList(list)
+	expected := []int{2, 4, 4, 5, 6, 7, 8}
+
+	if testList == nil {
+		t.Errorf("expected: '%d', got: '%d'", expected, testList)
+	}
+
+}
