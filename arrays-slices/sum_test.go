bc4995dcc867837405caaced54f5ce20abc2a56a
Rohan Gupta <srgupta5328@gmail.com>
2019-01-07 12:03:41 -0800

    finished slies and arrays chapter

--- arrays-slices/sum_test.go
+++ arrays-slices/sum_test.go
@@ -62,23 +62,23 @@ func TestSumAllV2(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
 
-	t.Run("Some of some slices ", func(t *testing.T) {
+	t.Run("make the sums of tails of", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("Want: '%v', Got: '%v'", want, got)
-		}
+		checkSums(t, got, want)
 	})
 
-	t.Run("Safely sum empty slices", func(t *testing.T) {
-		got := SumAllTails([]int{}, []int{3, 9})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("Want: '%v', Got: '%v'", want, got)
-		}
+		checkSums(t, got, want)
 	})
 
 }
