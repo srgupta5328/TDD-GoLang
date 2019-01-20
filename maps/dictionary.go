97c93428c0cedb36e632c309a9bdbe7111789134
Rohan Gupta <srgupta5328@gmail.com>
2019-01-19 16:43:08 -0800

    TDD with maps

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,7 @@
 package maps
 
+import "errors"
+
 func Search(dictionary map[string]string, word string) string {
 	for k, v := range dictionary {
 		if k == word {
@@ -10,4 +12,20 @@ func Search(dictionary map[string]string, word string) string {
 	}
 
 	return dictionary[word]
+}
+
+type Dictionary map[string]string
+
+var ErrorNotFound = errors.New("Couldn't find the word")
+
+func (d Dictionary) SearchV2(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrorNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, def string) {
+	d[word] = def
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -10,8 +10,8 @@ func assertString(t *testing.T, got string, want string) {
 	}
 }
 func TestSearch(t *testing.T) {
+	dictionary := map[string]string{"test": "this is just a test"}
 	t.Run("If the word is a match", func(t *testing.T) {
-		dictionary := map[string]string{"test": "this is just a test"}
 
 		got := Search(dictionary, "test")
 		want := "this is just a test"
@@ -20,7 +20,6 @@ func TestSearch(t *testing.T) {
 	})
 
 	t.Run("If Word wasn't found", func(t *testing.T) {
-		dictionary := map[string]string{"test": "this is just a test"}
 
 		got := Search(dictionary, "math")
 		want := "Couldn't find the word"
@@ -28,4 +27,51 @@ func TestSearch(t *testing.T) {
 		assertString(t, got, want)
 	})
 
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error '%s' want '%s'", got, want)
+	}
+}
+func TestSearchV2(t *testing.T) {
+	dictionary := Dictionary{"test": "This is a type test"}
+	t.Run("Known Word", func(t *testing.T) {
+
+		got, _ := dictionary.SearchV2("test")
+		want := "This is a type test"
+
+		assertString(t, got, want)
+	})
+
+	t.Run("Unknown word", func(t *testing.T) {
+
+		_, got := dictionary.SearchV2("unknown")
+
+		assertError(t, got, ErrorNotFound)
+	})
+
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "hot"
+	def := "having a high degree of heat or a high temperature"
+	dictionary.Add(word, def)
+
+	assertDefinition(t, dictionary, word, def)
+
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, def string) {
+	t.Helper()
+
+	got, err := dictionary.SearchV2(word)
+	if err != nil {
+		t.Fatalf("Should search for an added word: %s", err)
+	}
+	if def != got {
+		t.Errorf("Got: %s, Want: %s", got, def)
+	}
 }
