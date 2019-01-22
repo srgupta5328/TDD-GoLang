9a3692105abe42dfef568802f73b57dea81f8317
Rohan Gupta <srgupta5328@gmail.com>
2019-01-21 19:26:31 -0800

    dependency injections

---
+++ dependency-injections/greet.go
@@ -0,0 +1,18 @@
+package dependency
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreetingsHandler(w http.ResponseWriter, req *http.Request) {
+	Greet(w, "world")
+}
+func main() {
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreetingsHandler))
+}
