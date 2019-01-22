f2011fad25bfb6178018c14af29cf7c53baba09e
Rohan Gupta <srgupta5328@gmail.com>
2019-01-21 18:59:16 -0800

    refactoring failing test case

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -91,26 +91,24 @@ func assertDefinition(t *testing.T, dictionary Dictionary, word, def string) {
 func TestUpdate(t *testing.T) {
 	t.Run("existing word", func(t *testing.T) {
 		word := "test"
-		definition := "this is a test"
-
-		dictionary := Dictionary{word: definition}
+		definition := "this is just a test"
 		newDefinition := "new definition"
-
+		dictionary := Dictionary{word: definition}
 		err := dictionary.Update(word, newDefinition)
 
 		assertError(t, err, nil)
 		assertDefinition(t, dictionary, word, newDefinition)
 	})
+
 	t.Run("new word", func(t *testing.T) {
 		word := "test"
-		definition := "this is a test"
-		dictionary := Dictionary{word: definition}
+		definition := "this is just a test"
+		dictionary := Dictionary{}
 
 		err := dictionary.Update(word, definition)
 
 		assertError(t, err, ErrWordDoesNotExist)
 	})
-
 }
 
 func TestDelete(t *testing.T) {
